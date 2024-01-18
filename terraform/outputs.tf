5527db3cba09d17b26b22910ed8edb15848c877c
Berend-Jan Lange <berendjanlange@Berend-Jans-MacBook-Pro.local>
2024-01-18 18:45:30 +0100

    update playbook

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,17 +4,17 @@ output "aws_ami_name" {
   value       = data.aws_ami.debian.id
 }
 
-# output "aws_eip_public_dns" {
-#   description = "Instance EIP Public DNS"
-#   value       = module.forms.aws_eip_public_dns
-# }
+output "aws_eip_public_dns" {
+  description = "Instance EIP Public DNS"
+  value       = module.forms.aws_eip_public_dns
+}
 
-# output "aws_eip_public_ip" {
-#   description = "Public ip of elastic IP"
-#   value       = module.forms.aws_eip_public_ip
-# }
+output "aws_eip_public_ip" {
+  description = "Public ip of elastic IP"
+  value       = module.forms.aws_eip_public_ip
+}
 
-# output "aws_ebs_volume_id" {
-#   description = "EBS Volume Device Id"
-#   value       = module.forms.aws_ebs_volume_id
-# }
+output "aws_ebs_volume_id" {
+  description = "EBS Volume Device Id"
+  value       = module.forms.aws_ebs_volume_id
+}
