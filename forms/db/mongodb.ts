9b07d340aacd20f38f64e9c7ab38e55bab43a11c
Berend-Jan Lange <berendjanlange@Berend-Jans-MacBook-Pro.local>
2024-01-28 12:12:46 +0100

    update workflow

--- forms/db/mongodb.ts
+++ forms/db/mongodb.ts
@@ -1,16 +1,18 @@
 import { MongoClient } from 'mongodb'
 
-if (!process.env.MONGODB_URI) {
+const env = process.env
+
+if (!env.MONGODB_URI) {
     throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
 
-const uri = process.env.MONGODB_URI
+const uri = env.MONGODB_URI
 const options = {}
 
 let client
 let clientPromise: Promise<MongoClient>
 
-if (process.env.NODE_ENV === 'development') {
+if (env.NODE_ENV === 'development') {
     // In development mode, use a global variable so that the value
     // is preserved across module reloads caused by HMR (Hot Module Replacement).
     let globalWithMongo = global as typeof globalThis & {
