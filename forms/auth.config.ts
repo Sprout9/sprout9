846cf2c1b24f1ee6be8bd44237df9967ad43f80f
Berend-Jan Lange <berendjanlange@Berend-Jans-MacBook-Pro.local>
2024-02-03 21:06:41 +0100

    update forms

--- forms/auth.config.ts
+++ forms/auth.config.ts
@@ -6,6 +6,7 @@ export const authConfig = {
     providers: [],
     pages: {
         signIn: '/login',
+        newUser: '/create-account',
     },
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
@@ -13,9 +14,12 @@ export const authConfig = {
             const isOnForms = nextUrl.pathname.startsWith('/forms')
             const isResponding = isRespondingRegex.test(nextUrl.pathname)
             const isOnAccount = nextUrl.pathname === '/account'
+            const isBase = nextUrl.pathname === ''
+
+            if (isResponding || isBase) return true
 
             if (isOnForms || isOnAccount) {
-                if (isResponding || isLoggedIn) return true
+                if (isLoggedIn) return true
                 return false // Redirect unauthenticated users to login page
             } else if (isLoggedIn) {
                 return Response.redirect(new URL('/forms', nextUrl));
